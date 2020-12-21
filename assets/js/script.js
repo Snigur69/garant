c804c1b76d0a9a9f56ad8059e1160708ef1e0462
Snigur69 <asnigur69@gmail.com>
2020-12-21 12:56:03 +0200

    added mob + fixed some issues

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,27 +6,23 @@ jQuery(function($) {
         centerPadding: '118px',
         slidesToShow: 3,
         dots: false,
-        arrows: true
-        // responsive: [
-        //     {
-        //         breakpoint: 768,
-        //         settings: {
-        //         arrows: false,
-        //         centerMode: true,
-        //         centerPadding: '40px',
-        //         slidesToShow: 3
-        //         }
-        //     },
-        //     {
-        //         breakpoint: 480,
-        //         settings: {
-        //         arrows: false,
-        //         centerMode: true,
-        //         centerPadding: '40px',
-        //         slidesToShow: 1
-        //         }
-        //     }
-        // ]
+        arrows: true,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    centerMode: false,
+                    centerPadding: '0px',
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+        ]
     });
 
 // =========================== SLIDERS ===========================
@@ -71,12 +67,21 @@ $('#main_filter .custom_dropdown_ul li').on('mousedown', function() {
     $(this).parent().parent().parent().find('.filter_input').val($(this).text());
 })
 
+$('.hidden_checkbox_menu .checkbox > .custom-checkbox'). on('change', function() {
+    let checked = $(this).parent().parent().parent().find('input:checked');
+    let values = [];
+    for(let i = 0; i < checked.length; i++) {
+        values.push($(checked[i]).val());
+    }
+    $('#type').val(values.join(' / '));
+})
+
 $('.price_wallet .wallet').on('click', function() {
     $('.price_wallet .wallet').removeClass('active');
     $(this).addClass('active');
     let wallet = $(this).text();
-    $('#price_to').attr('data-wallet', wallet)
-    $('#price_to').val($('#price_to').val().slice(0, -1) + wallet)
+    $('.input_price_wallet').text(wallet);
+    // $('#price_to').val($('#price_to').val().slice(0, -1) + wallet)
 })
 
 $('#price_from_vars > li').on('click', function() {
@@ -84,9 +89,21 @@ $('#price_from_vars > li').on('click', function() {
     $('#price_from').val($(this).data('value'));
 })
 
+$('#hidden_price_from').mask('000 000 000 000', {reverse: true}); 
+$('#hidden_price_to').mask('000 000 000 000', {reverse: true}); 
+
+$('#hidden_price_from').on('change', function() {
+    $('#price_from').val($(this).val());
+})
+
+$('#hidden_price_to').on('change', function() {
+    $('#price_to').val($(this).val());
+})
+
+
 $('#price_to_vars > li').on('click', function() {
     $('#hidden_price_to').val($(this).data('value'));
-    $('#price_to').val($(this).data('value') + ' ' + $('#price_to').data('wallet'));
+    $('#price_to').val($(this).data('value'));
 })
 
 $('.filter_top > .price').on('click', function(e) {
@@ -195,7 +212,19 @@ $(document).mousedown(function (e) {
 
     // =========================== FORM COUNTRY SELECT ===========================
     
-    $(document).on('click', '.hidden_countries > .country', function() {
+    $(document).on('click', '.hidden_modal_counties > .modal_country', function() {
+        let current = $('.country_icon .selected_modal_country').clone(true);
+        let selected = $(this).clone();
+        
+        $('.selected_modal_country').replaceWith(selected);
+        $(selected).addClass('selected_modal_country').removeClass('modal_country');
+        $(this).replaceWith(current);
+        $(current).removeClass('selected_modal_country').addClass('modal_country');
+        setMask('#modal_form_phone', '.selected_modal_country');
+    })
+
+
+    $(document).on('click', '.hidden_about_countries > .country', function() {
         let current = $('.country_icon .selected_country').clone(true);
         let selected = $(this).clone();
         
@@ -203,42 +232,77 @@ $(document).mousedown(function (e) {
         $(selected).addClass('selected_country').removeClass('country');
         $(this).replaceWith(current);
         $(current).removeClass('selected_country').addClass('country');
-        setMask();
+        setMask('#form_phone', '.selected_country');
     })
-    setMask();
+
+
+    setMask('#modal_form_phone', '.selected_modal_country');
+    setMask('#form_phone', '.selected_country');
     // =========================== FORM COUNTRY SELECT ===========================
 
 
     // =========================== MASK ===========================
-    function setMask () {
-        let code = $('.selected_country').data('value');
+    function setMask (id, selected) {
+        let code = $(selected).data('value');
         switch(code) {
             case ('ua'): {
-                $('#form_phone').mask('+3 (000) 000 00 00');
-                $('#form_phone').attr('placeholder', '+3 (999) 999 99 99')
-                $('#form_phone').val('');
+                $(id).mask('+3 (000) 000 00 00');
+                $(id).attr('placeholder', '+3 (999) 999 99 99')
+                $(id).val('');
                 break;
             }
             case ('ru'): {
-                $('#form_phone').mask('+7 (000) 000 00 00');
-                $('#form_phone').attr('placeholder', '+7 (999) 999 99 99')
-                $('#form_phone').val('');
+                $(id).mask('+7 (000) 000 00 00');
+                $(id).attr('placeholder', '+7 (999) 999 99 99')
+                $(id).val('');
                 break;
             }
             case ('kz'): {
-                $('#form_phone').mask('8 (000) 000 00 00');
-                $('#form_phone').attr('placeholder', '8 (999) 999 99 99')
-                $('#form_phone').val('');
+                $(id).mask('8 (000) 000 00 00');
+                $(id).attr('placeholder', '8 (999) 999 99 99')
+                $(id).val('');
                 break;
             }
             default: {
-                $('#form_phone').mask('+3 (000) 000 00 00'); 
-                $('#form_phone').attr('placeholder', '+3 (999) 999 99 99')
-                $('#form_phone').val('');
+                $(id).mask('+3 (000) 000 00 00'); 
+                $(id).attr('placeholder', '+3 (999) 999 99 99')
+                $(id).val('');
             }
         }
     }
     // =========================== MASK ===========================
+
+
+
+    // =================== MOBILE MENU ========================
+    $('.button_menu').click(function(){
+        $(this).toggleClass('active'); 
+        $('.menu_responsive').toggleClass('active');
+      });
+
+      $(document).mouseup(function (e) {
+        if($('.menu_responsive').hasClass('active')){
+            var container = $(".menu_responsive");
+            if ((container.has(e.target).length === 0)){
+                $('.button_menu').click();
+            }
+            if ($(e.target).parent().hasClass('button_menu')){
+                $('.button_menu').click();
+            }
+            if ($(e.target).hasClass('button_menu')){
+                $('.button_menu').click();
+            }
+
+        };
+    });
+    $('.nav').on('click', function() {
+        if($('.menu_responsive').hasClass('active')) {
+            $('.button_menu').click();
+        }
+    });
+    
+    // =================== MOBILE MENU ========================
+
 
 
 });
